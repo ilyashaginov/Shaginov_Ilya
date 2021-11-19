26f525bb62dcc05e52d514da9f0dbca66f5ba199
ilyashaginov <shaginovilya@mail.ru>
2021-11-19 16:18:50 +0300

    тест репозитория 1

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 print 'Hello. I am a python program.'
 name = raw_input("What is your name?")
-print "Hello there, "+ name +"!"
+print "Hello there, " + name вваывапчясмывпчспяычпмяывпячыаи+ "!"
